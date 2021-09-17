c703ddeba4ee60beb2e22421030e954b1ed4ae9e
coderaulia <auliasw89@gmail.com>
2021-09-17 22:56:39 +0700

    starting data structures

---
+++ big-o/tweets.js
@@ -0,0 +1,10 @@
+// Find the 1th and the Nth... (last value)
+const array = [
+	{ tweet: "helo", date: 2020 },
+	{ tweet: "lagi apa ngab?", date: 2020 },
+	{ tweet: "happy new year!", date: 2021 },
+];
+// O(n^2)
+
+const array = ["hello", "lagi apa", "happy new year"];
+array[1]; // O(1)
---
+++ data-structure/README.md
@@ -0,0 +1,3 @@
+# Data Structures
+
+Basically, Data Structure is a way we can store, manage, and organizing data so it can be used effectively.
